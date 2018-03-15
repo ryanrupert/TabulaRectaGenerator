2d7e8b261f6c7f947493b3c59b7c00106d82007a
Ryan Rupert <mudge25+code@gmail.com>
2018-03-14 18:00:11 -0700

    Fix the tab spacing in source.cpp

--- src/Source.cpp
+++ src/Source.cpp
@@ -22,90 +22,90 @@ void error(std::string msg);
 
 int main(int argc, char** argv)
 {
-  srand(time(0));
-  int letter_range = 0;
-  bool rSet = false;
+	srand(time(0));
+	int letter_range = 0;
+	bool rSet = false;
 
-  //place the command argument code here
-  for (int i = 0; i < argc; i++) 
-  {
-  	if (!strcmp(argv[i], "-n")) 
-  	{
-  		rSet = true;
-		i++;
-		letter_range = atoi(argv[i]);
-		if (letter_range <= 0) 
+	//place the command argument code here
+	for (int i = 0; i < argc; i++) 
+	{
+		if (!strcmp(argv[i], "-n")) 
 		{
-			error("the number must be non-zero");
+			rSet = true;
+			i++;
+			letter_range = atoi(argv[i]);
+			if (letter_range <= 0) 
+			{
+				error("the number must be non-zero");
+				return 1;
+			}
+		}
+		else if (!strcmp[i], "-h" || !strcmp(argv[i], "--help"))
+		{
+			help();
+			return 0;
+		}
+		else
+		{
+			error();
 			return 1;
 		}
-  	}
-	else if (!strcmp[i], "-h" || !strcmp(argv[i], "--help"))
-	{
-		help();
-		return 0;
 	}
-	else
-	{
-		error();
-		return 1;
-	}
-  }
 
-  std::cout << "How many letters and numbers would you like the table to have?"
-  << " Must be less than or equal to " << letters.length() << std::endl;
-  std::cin >> letter_range;
-  std::cin.get();
+	std::cout << "How many letters and numbers would you like the table to have?"
+	<< " Must be less than or equal to " << letters.length() << std::endl;
+	std::cin >> letter_range;
+	std::cin.get();
 
-  std::cout << "Tabula Recta:" << std::endl;
-  std::cout << std::endl;
-  gen(letter_range);
-  std::cin.get();
-  return 0;
+	std::cout << "Tabula Recta:" << std::endl;
+	std::cout << std::endl;
+	gen(letter_range);
+	std::cin.get();
+	return 0;
 }
 
 std::string value()
 {
-  std::string temp;
-  int tempint;
+	std::string temp;
+	int tempint;
 
-  tempint = rand()%values.length();
-  temp = values.at(tempint);
-  return temp;
+	tempint = rand()%values.length();
+	temp = values.at(tempint);
+	return temp;
 }
 
 void line(int length)
 {
-  int index = 0;
+	int index = 0;
 
-  while(index < length)
-  {
-    std::cout << value() << "   ";
-    index++;
-  }
-  std::cout << std::endl;
+	while(index < length)
+	{
+		std::cout << value() << "   ";
+		index++;
+	}
+	std::cout << std::endl;
 }
 
 void gen(int letter_range)
 {
-  int index = 0;
+	int index = 0;
 
-  std::cout << "  |";
-  while (letter_range > index) {
-    std::cout << " " << letters.at(index) << " ";
-    if(index >= letter_range)
-      break;
-    std::cout << "|";
-    ++index;
-  }
-  std::cout << std::endl;
-  index = 0;
-  while(letter_range > index)
-  {
-    std::cout << letters.at(index) << " | ";
-    line(letter_range);
-    ++index;
-  }
+	std::cout << "  |";
+	while (letter_range > index) {
+		std::cout << " " << letters.at(index) << " ";
+		if(index >= letter_range)
+			break;
+		std::cout << "|";
+		++index;
+	}
+	std::cout << std::endl;
+	index = 0;
+	while(letter_range > index)
+	{
+		std::cout << letters.at(index) << " | ";
+		line(letter_range);
+		++index;
+	}
 }
 
 void help()

91e5c7f111245ab04f935686703b2e6d1c4711e0
Ryan Rupert <mudge25+code@gmail.com>
2018-03-14 17:39:39 -0700

    Add command line arguments to Source.cpp
    This adds all the required functions and if's for command line
    arguments.

--- src/Source.cpp
+++ src/Source.cpp
@@ -14,11 +14,41 @@ void line(int length);
 std::string values = "abcdefghijklmnopqrstuvwxyz0123456789()`~!@#$%^&*-+=|{}[]:;\"\'<>,.?/";
 void gen(int letter_range);
 std::string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+void help();
+void error();
+void error(std::string msg);
 
-int main()
+int main(int argc, char** argv)
 {
   srand(time(0));
   int letter_range = 0;
+  bool rSet = false;
+
+  //place the command argument code here
+  for (int i = 0; i < argc; i++) 
+  {
+  	if (!strcmp(argv[i], "-n")) 
+  	{
+  		rSet = true;
+		i++;
+		letter_range = atoi(argv[i]);
+		if (letter_range <= 0) 
+		{
+			error("the number must be non-zero");
+			return 1;
+		}
+  	}
+	else if (!strcmp[i], "-h" || !strcmp(argv[i], "--help"))
+	{
+		help();
+		return 0;
+	}
+	else
+	{
+		error();
+		return 1;
+	}
+  }
 
   std::cout << "How many letters and numbers would you like the table to have?"
   << " Must be less than or equal to " << letters.length() << std::endl;
@@ -74,4 +104,25 @@ void gen(int letter_range)
     line(letter_range);
     ++index;
   }
+}
+
+void help()
+{
+	std::cout << "usage: a [arguments]" << std::endl;
+	std::cout << std::endl;
+	std::cout << "arguments:" << std::endl;
+	std::cout << "-n [number]		Generate table with the number given." << std::endl;
+	std::cout << "-h, --help		Display help screen." << std::endl;
+}
+
+void error()
+{
+	std::cout << "argument not recognized." << std::endl;
+	help();
+}
+
+void error(std::string msg)
+{
+	std::cout << msg << std::endl;
+	help();
 }
